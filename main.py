8b885a88b4fcdee354c2ab195b634e4c24f3620c
Svahnen <svahn.jonny@gmail.com>
2021-09-22 13:45:31 +0200

    Create 2 rooms and functions for creating the world

--- main.py
+++ main.py
@@ -2,6 +2,23 @@
 
 from room import Room
 
-expeditionRoom = Room("expedition")
+def createWorld ():
+    rooms = [] # This will be floors in the future that return all possible rooms
+    rooms.append(Room("expedition"))
+    rooms.append(Room("office"))
+    currentRoom = rooms[0]
+    print("Location: "+ currentRoom.getName())
+    currentRoom = walk(rooms)
+    print("Location: "+ currentRoom.getName())
 
-print(expeditionRoom.getName())
+def walk(rooms):
+    print("The room names are:")
+    for room in rooms:
+        print(room.getName())
+    roomName = input("Where do you want to go? ")
+    for room in rooms:
+        if roomName == room.getName():
+            return room
+
+createWorld()
+
