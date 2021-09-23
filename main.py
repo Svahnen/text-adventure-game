e8ceba9325c22a0f7cbc721a144c39531c6cfd03
Svahnen <svahn.jonny@gmail.com>
2021-09-23 12:15:53 +0200

    Create doorAction

--- main.py
+++ main.py
@@ -19,12 +19,19 @@ def walk(rooms,currentRoom):
     for door in potentialDoors:
         print(door.getName())
     userInput = input("Where do you want to go: ")
-    for door in potentialDoors:
-        if userInput == door.getName() :
-            print(door.getName())
-            
+    for index in range(len(rooms)) :
+        if rooms[index].getName() == userInput :
+             return rooms[index]
         
-
+def doorAction(currentRoom):
+    print("These are the following door(s)")
+    potentialDoors = currentRoom.getDoorList()
+    for door in potentialDoors:
+        print(door.getName())
+    userInput = input("Which door do you want to open: ")
+    for index in range(len(potentialDoors)) :
+        if potentialDoors[index].getName() == userInput :
+             potentialDoors[index].open()
 
 def action(rooms):
     currentRoom = rooms[0]
@@ -32,10 +39,13 @@ def action(rooms):
         print("You are currently in " + currentRoom.getName())
         print("What do you want to do?")
         print("1. Walk")
+        print("2. Look for doors")
         try:
             action = int(input())
             if action == 1 :
                 currentRoom = walk(rooms, currentRoom)
+            elif action == 2 :
+                doorAction(currentRoom)
         except ValueError:
             print("You need to enter a number!")
             
